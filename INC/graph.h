f7cdbe2aaa8433a1bb0ee9fd1f5204ff8f957ac1
Kaldoran <kaldoran@live.fr>
2014-10-28 20:16:51 +0100

    Add a bit of commentary

--- INC/graph.h
+++ INC/graph.h
@@ -10,11 +10,25 @@
 
 #include "struct_graph.h"
 
+/** Allote a graph and the space for all node [ and all node without complete them ]
+ *  %param number_of_node : Number of node in the graph
+ */
 Graph new_graph(const int number_of_node);
 
+/** free a graph
+ *  %param g : graph to free
+ */
 void free_graph(Graph g);
 
+/** Print a graph
+ *  %param g : graph to print
+ */
 void print_graph(const Graph g);
+
+/** Reset the coloration of all node of a graph
+ *  %param g : graph to reset
+ */
+void reset_coloration(Graph g);
 
 
 #endif /* GRAPH_H included */
