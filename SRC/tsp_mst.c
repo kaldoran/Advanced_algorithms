8b84cc37c99053d411fb1cb545eaf7a98ae39b45
kevL974 <kev.laoussing@gmail.com>
2014-11-29 17:40:17 +0100

    construction de mst sans segfault

--- SRC/tsp_mst.c
+++ SRC/tsp_mst.c
@@ -17,18 +17,22 @@
 
 void MST(Graph g) {
 	int total_node = 1, i , j, min = 0, index = -1, index2 = -1;
-	Solution s = new_solution(g->count_nodes );
-	Graph minimal_spanning_tree = new_graph(g->count_nodes);
-	set_node(minimal_spanning_tree->nodes[0], 0, g->count_nodes);
+	Solution s = new_solution(g->count_nodes );						//Create solution
+	Graph minimal_spanning_tree = new_graph(g->count_nodes); 		//create minimal spanning tree
+	set_node(minimal_spanning_tree->nodes[0], 0, g->count_nodes);	//add root node in mst
 
-	add_node(s, g->nodes[0], 0);
+	add_node(s, g->nodes[0], 0);									//add root node in solution;
 	printf("De : Nom [Indice tableau dans S] - Nom [indice tableau dans g]\n");
+	/** while we don't see all node in graph g */
 	while( total_node != g->count_nodes) {
-		min = INT_MAX;
+		min = INT_MAX;						//initiate min
+		/** foreach node we already see */
 		for ( i = 0; i < total_node;i++ ) {
+			/** find minimal node cost */
+			/** foreach subnodes of node[i] containing in solution */
 			for ( j = 0; j < s->list_node[i]->count_subnodes; j++) {
-				if (  s->list_node[i]->subnodes[j] != NULL 
-				  && s->list_node[i]->cost[j] < min 
+				if (  s->list_node[i]->subnodes[j] != NULL  		//if subnode[j] exist
+				  && s->list_node[i]->cost[j] < min 				//if cost of subnode[j] < min
 				  && !contains(s, s->list_node[i]->subnodes[j]) ){
 					min = s->list_node[i]->cost[j];
 					index = j;
@@ -39,30 +43,24 @@ void MST(Graph g) {
 		}
 		
 		printf("De : %d [%d] - %d [%d]\n", s->list_node[index2]->name, index2, g->nodes[index]->name, index);
+
+		/*minimal_spanning_tree->nodes[index2] = s->list_node[index2];
+		minimal_spanning_tree->nodes[index] = g->nodes[index];
 		set_node(minimal_spanning_tree->nodes[index2], s->list_node[index2]->name,g->count_nodes);
+		set_node(minimal_spanning_tree->nodes[index], g->nodes[index]->name,g->count_nodes);*/
+
+		minimal_spanning_tree->nodes[index] = new_node();
 		set_node(minimal_spanning_tree->nodes[index], g->nodes[index]->name,g->count_nodes);
 
 		/**building*/
-		minimal_spanning_tree->nodes[index2]->subnodes[index] = minimal_spanning_tree->nodes[index];
-		minimal_spanning_tree->nodes[index]->subnodes[index2] = minimal_spanning_tree->nodes[index2];
-
+		minimal_spanning_tree->nodes[s->list_node[index2]->name]->subnodes[g->nodes[index]->name] = minimal_spanning_tree->nodes[g->nodes[index]->name];
+		minimal_spanning_tree->nodes[g->nodes[index]->name]->subnodes[s->list_node[index2]->name] = minimal_spanning_tree->nodes[s->list_node[index2]->name];
 
 		add_node(s, g->nodes[index], 0);
 		++total_node;
 	}
 
 	print_graph(minimal_spanning_tree);
-	for (i = 0; i < g->count_nodes; i++) {
-		if(minimal_spanning_tree->nodes[i] != NULL) {
-			printf("	");
-			for (j = 0; j < g->count_nodes; j++) {
-				if(minimal_spanning_tree->nodes[i]->subnodes[j] != NULL ) {
-					printf("De : %d - %d\n", minimal_spanning_tree->nodes[i]->name, minimal_spanning_tree->nodes[i]->subnodes[j]->name);	
-				}
-				
-			}	
-		}
-	}
 
 	prefix_course(minimal_spanning_tree->nodes[0]);
 }
@@ -81,6 +79,6 @@ void prefix_course(Node tree) {
 				}
 			}
 		}
-		//printf("feuille : %d\n",tree->subnodes[i] );
+		printf("feuille : %d\n",tree->subnodes[i]->name );
 	}
 }
