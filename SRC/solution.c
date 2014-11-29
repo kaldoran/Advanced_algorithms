9228825185d4fb2efd244a7189e89e4badb4e3da
Nicolas <kaldoran@users.noreply.github.com>
2014-11-29 23:36:05 +0100

    Update solution.c

--- SRC/solution.c
+++ SRC/solution.c
@@ -78,12 +78,12 @@ Solution best_solution( Solution* list_solution, int nb_solution) {
 	}
 
 	/**free fake solutions */
-/*	for ( i=0; i< nb_solution; i++) {
+	for ( i=0; i< nb_solution; i++) {
 		if ( i != ref ) {
 			free_solution(list_solution[i]);
 		}
 	}
-*/
+
 	return list_solution[ref];
 }
 
